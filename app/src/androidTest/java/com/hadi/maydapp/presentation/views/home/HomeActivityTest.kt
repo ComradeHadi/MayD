0d2e7b67b6b76953f0a2cee02b500f26eb564448
ComradeHadi <comradehadi@ramani.io>
2022-12-08 10:29:53 +0000

    Added instrumentation tests

--- app/src/androidTest/java/com/hadi/maydapp/presentation/views/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/hadi/maydapp/presentation/views/home/HomeActivityTest.kt
@@ -1,13 +1,17 @@
 package com.hadi.maydapp.presentation.views.home
 
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import com.hadi.maydapp.R
 import com.hadi.maydapp.presentation.views.Home.HomeActivity
+import kotlinx.android.synthetic.main.activity_short_link_intro.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,11 +28,27 @@ class HomeActivityTest {
         activityRule.launchActivity(null)
     }
 
+    /**
+     * Please note only one of the following test will pass at any given time since they both can't happen.
+     * To test the last case, one should have no item in the local database
+     *
+     */
+
     @Test
     fun onHomeActivityLaunchedConfirmRecyclerViewIsDisplayed() {
         activityRule.launchActivity(null)
-        onView(withId(R.id.teams_recycler_view_list))
-            .check(matches(isDisplayed()))
+        if(activityRule.activity.EMPTY_STATE)
+            Espresso.onView(withId(R.id.empty_links_page))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+
+    @Test
+    fun onHomeActivityLaunchedWithErrorGIFIsDisplayed() {
+        activityRule.launchActivity(null)
+        if(!activityRule.activity.EMPTY_STATE)
+            Espresso.onView(withId(R.id.recylerViewLinks))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
 }
--- app/src/main/java/com/hadi/maydapp/presentation/views/Home/HomeActivity.kt
+++ app/src/main/java/com/hadi/maydapp/presentation/views/Home/HomeActivity.kt
@@ -15,19 +15,17 @@ import javax.inject.Inject
 class HomeActivity : DaggerAppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
-    private lateinit var PATTERN: Pattern
+    var EMPTY_STATE: Boolean = false
     private lateinit var homeViewModel: HomeViewModel
     private lateinit var paymentPageBinding: ActivityShortLinkIntroBinding
-    private var selectedCurrency: String? = null
     private lateinit var selectedCountry: String
-    private var sendingAmount: Long? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         paymentPageBinding = ActivityShortLinkIntroBinding.inflate(layoutInflater)
 
         setContentView(paymentPageBinding.root)
-//        Log.e("crush", selectedCountry.toString())
+        Log.e("crush", selectedCountry.toString())
 
         paymentPageBinding = ActivityShortLinkIntroBinding.inflate(layoutInflater)
         homeViewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
@@ -38,17 +36,17 @@ class HomeActivity : DaggerAppCompatActivity() {
         recylerViewLinks.adapter = historyListAdapter
 
         observeServerResponse()
-        showLinkHistory()
 
         shorten_button.setOnClickListener{
             val textTo = input_edit_text.text.toString()
             Toast.makeText(this@HomeActivity, textTo, Toast.LENGTH_LONG).show()
             homeViewModel.getShortenedUrl(textTo)
+            showLinkHistory()
         }
 
         homeViewModel.savedLinks.observe(this, {
             if(it.size != 0) {
-                Log.e("11111111 test 1",  it.size.toString())
+                Log.e("11111111 changes 1",  it.size.toString())
                 empty_links_page.visibility = View.GONE
                 historyListAdapter.providelinkHistoryList(it.map { eachItem -> eachItem.toLinkUIModel() })
                 recylerViewLinks.visibility = View.VISIBLE
@@ -72,7 +70,9 @@ class HomeActivity : DaggerAppCompatActivity() {
                 Log.e("11111111 data",  it.data.toString())
                 /** All actions here
                  * */
+
                 input_edit_text.text.clear()
+                showLinkHistory()
 
             }
 
@@ -96,89 +96,8 @@ class HomeActivity : DaggerAppCompatActivity() {
 
                 empty_links_page.visibility = View.VISIBLE
                 recylerViewLinks.visibility = View.GONE
+                EMPTY_STATE = true
             }
         })
     }
-//
-//    fun test() {
-//        val n: Int = 75
-//
-//        val binary: String = Integer.toBinaryString(n)
-//
-//        println(binary)
-//    }
-//
-//    // Function to check Binary Number
-//    private fun isBinaryNumber(binaryNumber: Long): Boolean {
-//        var binaryNumber = binaryNumber
-//        while (binaryNumber > 0) {
-//            if (binaryNumber % 10 > 1) {
-//                return false
-//            }
-//            binaryNumber /= 10
-//        }
-//        return true
-//    }
-//
-//    private fun convertBinaryToDecimal(num: Long): Long {
-//        var num = num
-//        var decimalNumber = 0L
-//        var i = 0
-//        var remainder: Long
-//
-//        while (num.toInt() != 0) {
-//            remainder = num % 10
-//            num /= 10
-//            decimalNumber += (remainder * Math.pow(2.0, i.toDouble())).toInt()
-//            ++i
-//        }
-//        return decimalNumber
-//    }
-//
-//    private fun getCurrency(country: String) = when (country) {
-//        "Kenya" -> "KES"
-//        "Nigeria" -> "NGN"
-//        "Tanzania" -> "TZS"
-//        "Uganda" -> "UGX"
-//        else -> null
-//
-//    }
-//
-//    private fun getRateForCurrency(currency: String, remoteRates: LinkUIModel) = when (currency) {
-//        "KES" -> remoteRates.kenyaRate
-//        "NGN" -> remoteRates.nigeriaRate
-//        "TZS" -> remoteRates.tanzaniaRate
-//        "UGX" -> remoteRates.ugandaRate
-//        else -> null
-//
-//    }
-//
-//    private fun calculateReceivingAmountInBinary(send: Long, rate: Double): String {
-//        val amountInDecimals = send * rate
-//        val binary = Integer.toBinaryString(amountInDecimals.toInt())
-//
-//        return binary.toString()
-//    }
-//
-//    private fun isPhoneNumberValid(number: String): Boolean {
-//
-//
-//        val expectedRegex = when (selectedCountry) {
-//            "Kenya" -> "^(\\+254[\\-\\s]?)?\\d{9}\$"
-//            "Nigeria" -> "^(\\+234[\\-\\s]?)?\\d{7}\$"
-//            "Tanzania" -> "^(\\+255[\\-\\s]?)?\\d{9}\$"
-//            "Uganda" -> "^(\\+256[\\-\\s]?)?\\d{9}\$"
-//            else -> null
-//        }
-//
-//        PATTERN = Pattern.compile(expectedRegex)
-//        val finalBoolean = number.isPhoneNumber()
-//
-//
-//
-//        return finalBoolean
-//
-//    }
-//
-//    fun CharSequence.isPhoneNumber(): Boolean = PATTERN.matcher(this).find()
 }
